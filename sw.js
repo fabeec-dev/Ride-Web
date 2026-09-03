d6acdad74fd58299634a18bda8d464d3b4212407
fabeec-dev <fabiosilva738@gmail.com>
2026-09-03 20:15:35 +0200

    Fix stale PWA assets after deployment
    Versions frontend assets and tolerates cached markup during service worker upgrades.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ride-pwa-v12';
+const CACHE='ride-pwa-v13';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./hero-road.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
